e514e242dcc4bd95db0555614c383f19e0080378
ViralL <viirall@yandex.ru>
2015-10-27 17:13:11 +0300

    del

--- clever/scripts/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-$(window).load(function () {
-    window.setInterval(function () {
-        $(".btn--danger").toggleClass("active");
-    }, 1000);
-});
-//# sourceMappingURL=main.js.map
