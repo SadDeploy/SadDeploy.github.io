4db017a6f9d8c78b61bd56c457a7012a7d5df602
ViralL <viirall@yandex.ru>
2015-11-25 20:56:18 +0300

    clubrent

---
+++ clubrent/scripts/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+$(window).load(function () {
+    $("#owl-example").owlCarousel({
+        navigation: true, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        navigationText: ["", ""],
+        singleItem: true
+    });
+});
+//# sourceMappingURL=main.js.map
